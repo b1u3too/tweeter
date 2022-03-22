07203df05baedec9097156a6cf27bd581574ec4e
b1u3too <almadsen792@live.ca>
2022-03-22 03:29:59 +0000

    remove console logs, readability adjustments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
-  console.log("JQuery loaded");
   $('#tweet-text').on('input', function(event) {
-    console.log(this);
-
+    //find counter element by traversing DOM tree
     const counterElement = $(this).parent().find('.counter');
+    
     const maxLength = counterElement.attr("maxLength");
     const currentLength = $(this).val().length;
     const charsRemaining = maxLength - currentLength;
-
-  
-    //$(this).parent() to get to form, and use find('.counter) to go find the element.. 
 
     counterElement.html(`${charsRemaining}`);
 
