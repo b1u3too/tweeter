4719aef0c2b0c6396bd0403afc3297c683547b79
b1u3too <almadsen792@live.ca>
2022-03-23 03:50:48 +0000

    jQuery dynamic tweet rendering - helper functions renderTweets and createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,61 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Hardcode proof of concept data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+function createTweetElement(tweetData) {
+  return `
+  <article>
+  <header>
+    <div class="user-markers">
+      <img src=${tweetData.user.avatars}>
+      <p>${tweetData.user.name}</p>
+    </div>
+    <p>${tweetData.user.handle}</p>
+  </header>
+  <p>${tweetData.content.text}</p> 
+  <footer>
+    <p>${tweetData.created_at}</p>
+    <div class="icon-buttons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+  </article>
+  `
+};
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+};
+
+$(document).ready(function() {
+  renderTweets(data);
+});
